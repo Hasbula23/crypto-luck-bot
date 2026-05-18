34bd7c8e2be904cda69b3b94cea6be5a06b31547
Hasbula23 <nikitasidorov575@gmail.com>
2026-05-18 09:43:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("NEW_BOT_TOKEN")  # Берём из переменной NEW_BOT_TOKEN
+BOT_TOKEN = "7681724637:AAHfeg0yCouLblIxpNNUfIXeJ4nCO0G4E3U"
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
