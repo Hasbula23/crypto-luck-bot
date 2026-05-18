ae64614543216ce5ed1273d6c1c188dd9dc6b603
Hasbula23 <nikitasidorov575@gmail.com>
2026-05-18 11:36:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = "8840502487:AAFPRgQgsxEC14CObDFJMSomMAAnRdEfLlg"
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = https://nzbebjfzdqntydmzcxaf.supabase.co/rest/v1/
+SUPABASE_KEY = sb_publishable_CFfsi2y7uhPwoIb21D19Vg_Ce3bfmFG
 ADMIN_ID = 7955348018
-CRYPTO_WALLET = os.getenv("CRYPTO_WALLET")
+CRYPTO_WALLET = TPm8A1BfsZQeB9izEE1C3ZNUCLveYx2tEU
 MIN_BET = 5
 REK = 0.05
 WEBAPP_URL = "https://verdant-tartufo-048198.netlify.app"
