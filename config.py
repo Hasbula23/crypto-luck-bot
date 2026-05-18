66c632719517dddbac8ccbf6ee61edbdbb1ff4b2
Hasbula23 <nikitasidorov575@gmail.com>
2026-05-18 16:10:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ CRYPTO_WALLET = "TPm8A1BfsZQeB9izEE1C3ZNUCLveYx2tEU"
 MIN_BET = 100
 REK = 0.1
 CURRENCY = "⭐️ Stars"
-WEBAPP_URL = "https://celadon-pie-5186e2.netlify.app/"
+WEBAPP_URL = "https://rococo-dieffenbachia-56ee82.netlify.app/"
