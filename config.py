4fb2db754e2b4279ae8a3393478b8f9b0d3571e1
Hasbula23 <nikitasidorov575@gmail.com>
2026-05-18 14:40:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ CRYPTO_WALLET = "TPm8A1BfsZQeB9izEE1C3ZNUCLveYx2tEU"
 MIN_BET = 100
 REK = 0.1
 CURRENCY = "⭐️ Stars"
-WEBAPP_URL = "https://effortless-brigadeiros-c21c44.netlify.app/"
+WEBAPP_URL = "https://celadon-pie-5186e2.netlify.app/"
