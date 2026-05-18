b91b9ecd637e6f29db901b60d0709887674d838c
Hasbula23 <nikitasidorov575@gmail.com>
2026-05-18 09:58:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 BOT_TOKEN = "7681724637:AAGqu_yhj_6DCMpIwgySdctogVF0dLHzweI"
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+ADMIN_ID = 7955348018
 CRYPTO_WALLET = os.getenv("CRYPTO_WALLET")
 MIN_BET = 5
 REK = 0.05
