8326ed98307e5999c61a4391e2ec891de76719ed
Hasbula23 <nikitasidorov575@gmail.com>
2026-05-18 13:08:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ SUPABASE_KEY = "sb_publishable_CFfsi2y7uhPwoIb21D19Vg_Ce3bfmFG"
 ADMIN_ID = 7955348018
 CRYPTO_WALLET = "TPm8A1BfsZQeB9izEE1C3ZNUCLveYx2tEU"
 MIN_BET = 5
-REK = 0.05
+REK = 0.1
 WEBAPP_URL = "https://verdant-tartufo-048198.netlify.app"
